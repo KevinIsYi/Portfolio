81ec9af1f784460f38291ef3112d888b867d031b
KevinIsYi <rodriguezkevin95g@gmail.com>
2021-07-12 19:11:51 -0500

    Added sidebar menu when screen is tiny

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -9,13 +9,12 @@ export const Background = () => {
         const figures = ['shape', 'shape circle', 'triangle'];
         const colors = ['#ef0f73', '#ff6803', '#333399'];
 
+        const numberOfShapes = Math.round(innerWidth / 85);
         const fillAnimations = [];
-        const numberOfFigures = figures.length;
-        const numberOfShapes = Math.round(innerWidth / 50);
-
+        
         for (let key = 1; key <= numberOfShapes; ++key) {
             const random = Math.random();
-            const figurePosition = Math.floor(random * numberOfFigures); // [0, figures.length - 1]
+            const figurePosition = key % 3; // [0, figures.length - 1]
             const figure = figures[figurePosition]; // Name of a figure
             const animationDelay = `${random * 50}s`; // Random number between [0, 50]
             const left = Math.floor(random * innerWidth); // position: fixed; left: ?
@@ -44,7 +43,7 @@ export const Background = () => {
                 animations.map(({ key, figure, style }) => (
                     <div
                         key={key}
-                        className={`anim ${figure}`}
+                        className={`fixed anim ${figure}`}
                         style={style}
                     />
                 ))
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,18 @@
 import { Link, NavLink } from 'react-router-dom';
+import { SidebarMenu } from '../../SidebarMenu/SidebarMenu';
 
 export const Header = () => {
 
     return (
         <header className="header__container flex roboto bold">
             <Link to="/" className="header__name text-center lobster gray-text">Kevin Rodríguez</Link>
-            <nav className="header__nav">
-                <NavLink to="/resume" exact activeClassName="active-link">Resumé</NavLink>
+            <nav className="header__nav hidden">
+                <NavLink to="/resume" exact activeClassName="active-link">Resume</NavLink>
                 <NavLink to="/projects" exact activeClassName="active-link">Projects</NavLink>
                 <NavLink to="/about" exact activeClassName="active-link">About</NavLink>
                 <NavLink to="/contact" exact activeClassName="active-link">Contact</NavLink>
             </nav> 
+            <SidebarMenu />
         </header>
     )
 }
